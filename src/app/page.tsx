26f1998363e0d5ae37e76b25e5d48fe9f13a47aa
Josh Mackus <josh.mackus@dayforce.com>
2026-02-28 14:32:17 +1100

    style: remove film grain effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,10 @@ import { HistoryPanel } from "@/components/studio/history-panel";
 import { GenerationControls } from "@/components/studio/generation-controls";
 import { ApiKeyDialog } from "@/components/studio/api-key-dialog";
 import { ImageViewer } from "@/components/studio/image-viewer";
-import { FilmGrain } from "@/components/studio/film-grain";
 
 export default function Home() {
   return (
     <div className="relative h-dvh w-full overflow-hidden bg-background">
-      <FilmGrain />
       <StudioHeader />
       <StudioCanvas />
       <HistoryPanel />
